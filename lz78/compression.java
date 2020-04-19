40907458dc063b91ca86f1f40b3b7986db9dea68
meeedo <36135264+meeedo@users.noreply.github.com>
2020-04-19 02:47:57 +0200

    Add files via upload

---
+++ lz78/compression.java
@@ -0,0 +1,17 @@
+package lz78;
+
+import java.util.ArrayList;
+import javatuples.Pair;
+
+public class compression {
+    String data;
+    ArrayList<Pair <String , Integer>> Dictionary = new ArrayList<Pair <String , Integer>>();
+    public void convert(String data)
+    {
+        this.data = data;
+        for(int i=0 ; i<data.length() ; i++)
+        {
+            
+        }
+    }
+}
